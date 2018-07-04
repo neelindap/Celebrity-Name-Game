1bd460a8357e3e031cb7e4563d3f00eaf04a2698
Neel Indap <neelindap@gmail.com>
2018-07-04 18:52:52 -0400

    Services added

--- src/app/services/data/user/user.service.ts
+++ src/app/services/data/user/user.service.ts
@@ -1,6 +1,7 @@
 import { Injectable } from '@angular/core';
 import { AngularFireDatabase } from 'angularfire2/database';
 import { User } from '../../../models/user.model';
+import { of } from 'rxjs';
 
 @Injectable({
   providedIn: 'root'
@@ -8,9 +9,11 @@ import { User } from '../../../models/user.model';
 export class UserService {
 
   private user: any;
+  private score: any;
 
   constructor(private firebase: AngularFireDatabase) { }
 
+  // Get logged in user details
   getUser(uid: string) {
     let ref = this.firebase.list('users');
     ref.snapshotChanges().subscribe(item => {
@@ -24,6 +27,7 @@ export class UserService {
     });
   }
 
+  // Return user object
   getUserObj() {
     return this.user;
   }
@@ -33,5 +37,21 @@ export class UserService {
 
   }
 
-  
+  // Update the user object -> Game score
+  updateUser(score: number) {
+    let ref = this.firebase.list('users');
+    ref.update(this.user.key, {
+      email: this.user.email,
+      name: this.user.name,
+      score: this.user.score + score
+    });
+
+    this.score = score;
+    console.log('neth '+this.score);
+  }
+
+  getFinalScore() {
+    return of(this.score);
+  }
+
 }
