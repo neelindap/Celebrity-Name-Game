a08347950679a87eab5d2ae8a91fe8d281f5a5d6
Neel Indap <neelindap@gmail.com>
2018-07-06 15:13:20 -0400

    Error page

--- src/app/components/error/error.component.ts
+++ src/app/components/error/error.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Location } from '@angular/common';
 
 @Component({
   selector: 'app-error',
@@ -7,10 +8,12 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ErrorComponent implements OnInit {
 
-  constructor() { }
+  constructor(private location: Location) { }
 
   ngOnInit() {
-    window.location.href = 'https://http.cat/404'
   }
 
+  goBack() {
+    this.location.back(); // <-- go back to previous location on cancel
+  }
 }
