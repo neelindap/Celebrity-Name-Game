52a276a73020c1263c78cb4733695e1c39077047
Neel Indap <neelindap@gmail.com>
2018-07-07 11:18:33 -0400

    Test cases

--- src/app/components/footer/footer.component.spec.ts
+++ src/app/components/footer/footer.component.spec.ts
@@ -1,25 +1,38 @@
 import { async, ComponentFixture, TestBed } from '@angular/core/testing';
 
 import { FooterComponent } from './footer.component';
+import { DebugElement } from '@angular/core';
+import { By } from '@angular/platform-browser';
 
 describe('FooterComponent', () => {
   let component: FooterComponent;
   let fixture: ComponentFixture<FooterComponent>;
+  let de: DebugElement;
 
   beforeEach(async(() => {
     TestBed.configureTestingModule({
-      declarations: [ FooterComponent ]
-    })
-    .compileComponents();
+      declarations: [FooterComponent]
+    }).compileComponents();
   }));
 
   beforeEach(() => {
     fixture = TestBed.createComponent(FooterComponent);
     component = fixture.componentInstance;
+    de = fixture.debugElement;
+
     fixture.detectChanges();
   });
 
   it('should create', () => {
     expect(component).toBeTruthy();
   });
+
+  it('should have a div tag with `© 2018 Copyright:`', () => {
+    expect(de.query(By.css('div')).nativeElement.innerHTML).toContain('© 2018 Copyright:')
+  })
+
+  it('should contain href to Neel\'s homepage', () => {
+    let protfolio = fixture.debugElement.query(By.css("a")).nativeElement.getAttribute('href');
+    expect(protfolio).toEqual("http://www.neelindap.com/");
+  })
 });
